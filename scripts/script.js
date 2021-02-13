b669823fcc91f916c4a73204665c82e48d3fe648
Jazmusic1234 <jcc001@ucsd.edu>
2021-02-12 17:16:42 -0800

    Fetching data

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,16 @@
 // Script.js
+let url = 'https://fakestoreapi.com/products';
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  if(window.localStorage.length == 1) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        for(const product of data.products) {
+          localStorage.setItem('item', JSON.stringify(product));
+        }
+      })  
+  }
+    
 });
