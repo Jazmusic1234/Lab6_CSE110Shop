effd1d9b0f45cab0518fbf63c6bcaca7ccf40ba2
Jazmusic1234 <jcc001@ucsd.edu>
2021-02-13 21:29:30 -0800

    Added cart memory and changed alerts

--- components/product-item.js
+++ components/product-item.js
@@ -77,7 +77,7 @@ class ProductItem extends HTMLElement {
         <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
         <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
         <p class="price">$109.95</p>
-        <button onclick="alert('Added to Cart!')">Add to Cart</button>
+        <button>Add to Cart</button>
       </li>
     `;
   }
--- scripts/script.js
+++ scripts/script.js
@@ -3,47 +3,54 @@ const url = 'https://fakestoreapi.com/products';
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
-  if(window.localStorage.getItem(1) != null) {
-    window.localStorage.clear();
+  if(window.localStorage.length > 1) {
+    let cartCount = window.localStorage.length - 20;
+    document.getElementById('cart-count').innerHTML = cartCount;
   }
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      for(const product of data) {
-        window.localStorage.setItem(product.id, JSON.stringify(product));
+  else if(window.localStorage.getItem(21) == null) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        for(const product of data) {
+          window.localStorage.setItem(product.title, JSON.stringify(product));
+        } 
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+
+  for(let i = 1; i < 20; i++) {
+    const product = JSON.parse(window.localStorage.getItem(i));
+    const id = product.id;
+    const image = product.image;
+    const imagealt = product.description;
+    const title = product.title;
+    const price = product.price;
+    const item = document.createElement('product-item');
+    item.shadowRoot.querySelector('img').setAttribute('src', image);
+    item.shadowRoot.querySelector('img').setAttribute('alt', imagealt);
+    item.shadowRoot.querySelector('.title').innerHTML = title;
+    item.shadowRoot.querySelector('.price').innerHTML = price;
+    if(window.localStorage.getItem(`cartItem${id}`)) {
+      item.shadowRoot.querySelector('button').innerHTML = 'Remove from Cart';
+    }
+    item.shadowRoot.querySelector('button').addEventListener("click", () => {
+      cartCount = parseInt(document.getElementById('cart-count').innerHTML);
+      if(window.localStorage.getItem(`cartItem${id}`)) {
+        alert('Removed from Cart!');
+        window.localStorage.removeItem(`cartItem${id}`);
+        document.getElementById('cart-count').innerHTML = cartCount - 1;
+        item.shadowRoot.querySelector('button').innerHTML = 'Add to Cart';
+      } else {
+        alert('Added to Cart!');
+        window.localStorage.setItem(`cartItem${id}`, id);
+        document.getElementById('cart-count').innerHTML = cartCount + 1;
+        item.shadowRoot.querySelector('button').innerHTML = 'Remove from Cart';
       }
-      for(let i = 1; i < 20; i++) {
-        const product = JSON.parse(window.localStorage.getItem(i));
-        const id = product.id;
-        const image = product.image;
-        const imagealt = product.description;
-        const title = product.title;
-        const price = product.price;
-        const item = document.createElement('product-item');
-        item.shadowRoot.querySelector('img').setAttribute('src', image);
-        item.shadowRoot.querySelector('img').setAttribute('alt', imagealt);
-        item.shadowRoot.querySelector('.title').innerHTML = title;
-        item.shadowRoot.querySelector('.price').innerHTML = price;
-        item.shadowRoot.querySelector('button').addEventListener("click", () => {
-          let cartCount = parseInt(document.getElementById('cart-count').innerHTML);
-          if(window.localStorage.getItem(`cartItem${id}`)) {
-            window.localStorage.removeItem(`cartItem${id}`);
-            document.getElementById('cart-count').innerHTML = cartCount - 1;
-            item.shadowRoot.querySelector('button').innerHTML = 'Add to Cart';
-          } else {
-            window.localStorage.setItem(`cartItem${id}`, id);
-            document.getElementById('cart-count').innerHTML = cartCount + 1;
-            item.shadowRoot.querySelector('button').innerHTML = 'Remove from Cart';
-          }
-        });
-        document.getElementById('product-list').appendChild(item);
-      } 
-    })
-    .catch(error => {
-      console.error('Error:', error);
     });
-
-
+    document.getElementById('product-list').appendChild(item);
+  }
 
 });
